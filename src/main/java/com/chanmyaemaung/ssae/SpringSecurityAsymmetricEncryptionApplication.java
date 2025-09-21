5f435f5bce122fbd726c258c6c6f09da9aab6f0c
Chen Lay <maungmaungyoker.mmy@gmail.com>
2025-09-22 00:57:12 +0700

    init: initialized spring project

---
+++ src/main/java/com/chanmyaemaung/ssae/SpringSecurityAsymmetricEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.chanmyaemaung.ssae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAsymmetricEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAsymmetricEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chanmyaemaung/ssae/SpringSecurityAsymmetricEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanmyaemaung.ssae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAsymmetricEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
